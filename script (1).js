6fe5453bb2f95f29e4e536cef273bca2e7422a1f
Chloe Bright <chloebright@Chloes-MacBook-Pro.local>
2024-03-29 22:20:02 -0400

    updates to formatting of html/css/js files

--- script (1).js
+++ script (1).js
@@ -1,3 +1,4 @@
+
 function responsiveNavScroll() {
   
   var x = document.getElementById("myTopnav");
@@ -10,3 +11,21 @@ function responsiveNavScroll() {
     x.className = "topnav";
   }
 }
+
+function handleText(){
+  var h4 = document.getElementById("h4");
+  var textinput = document.getElementById("input");
+  h4.innerHTML = textinput.value;
+
+  
+}
+
+// call handleText() when the user types in the input element
+function init() {
+    var input = document.getElementById("input");
+    input.addEventListener("keyup", handleText);
+    
+}
+
+// call init() when the page loads
+window.addEventListener("load", init);
